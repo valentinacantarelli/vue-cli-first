f250bd282e5f5311b07abac034159befc363b1f9
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-28 17:04:30 +0200

    aggiunta hero

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -15,8 +15,10 @@ export default {
 .hero{
     height: 50vh;
     width: 100%;
-   background-color: rgb(184, 212, 184);
-   padding-top:70px;
+   background-image: url("https://images.unsplash.com/photo-1579492450119-80542d516179?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80");
+   background-repeat: no-repeat;
+   background-size: 100%;
+   padding-top:110px;
 }
 h1{
     height: 80px;
